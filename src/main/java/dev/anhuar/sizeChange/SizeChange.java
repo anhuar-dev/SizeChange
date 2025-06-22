d5dcca6f3736668a83554bdaf03e33d8aa91ab81
Anhuar Ruiz <myclxss@outlook.com>
2025-06-22 07:08:19 -0600

    add: structure files

---
+++ src/main/java/dev/anhuar/sizeChange/SizeChange.java
@@ -0,0 +1,17 @@
+package dev.anhuar.sizeChange;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SizeChange extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/dev/anhuar/sizeChange/handler/ManagerHandler.java
@@ -0,0 +1,17 @@
+package dev.anhuar.sizeChange.handler;
+
+/*
+ * ========================================================
+ * SizeChange - ManagerHandler.java
+ *
+ * @author Anhuar Ruiz | Anhuar Dev | myclass
+ * @web https://anhuar.dev
+ * @date 22/06/2025
+ *
+ * License: MIT License - See LICENSE file for details.
+ * Copyright (c) 2025 Anhuar Dev. All rights reserved.
+ * ========================================================
+ */
+public class ManagerHandler {
+
+}
